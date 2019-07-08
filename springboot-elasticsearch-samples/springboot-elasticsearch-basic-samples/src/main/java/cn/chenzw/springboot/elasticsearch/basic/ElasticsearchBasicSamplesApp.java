98c019a8e8547b30457efcd1d92b017033f8d67c
chenzw <656469722@qq.com>
2019-07-08 23:21:19 +0800

    initialization

---
+++ springboot-elasticsearch-samples/springboot-elasticsearch-basic-samples/src/main/java/cn/chenzw/springboot/elasticsearch/basic/ElasticsearchBasicSamplesApp.java
@@ -0,0 +1,12 @@
+package cn.chenzw.springboot.elasticsearch.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchBasicSamplesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchBasicSamplesApp.class, args);
+    }
+}
