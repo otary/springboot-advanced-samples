d8f1a841ca78d1a979a5e4d95f7c489523a7b2b3
chenzw <656469722@qq.com>
2019-12-10 12:55:05 +0800

    initialization

---
+++ springboot-hbase-samples/springboot-hbase-basic-samples/src/main/java/cn/chenzw/springboot/hbase/basic/HBaseBasicSamplesApp.java
@@ -0,0 +1,12 @@
+package cn.chenzw.springboot.hbase.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HBaseBasicSamplesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HBaseBasicSamplesApp.class, args);
+    }
+}
