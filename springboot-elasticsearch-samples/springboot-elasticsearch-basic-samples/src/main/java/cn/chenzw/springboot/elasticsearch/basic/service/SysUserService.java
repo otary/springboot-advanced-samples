fc029ae7b11befc670e83df6259e7f21e6dcfb23
chenzw <656469722@qq.com>
2020-08-14 22:59:13 +0800

    initialization

--- springboot-elasticsearch-samples/springboot-elasticsearch-basic-samples/src/main/java/cn/chenzw/springboot/elasticsearch/basic/service/SysUserService.java
+++ springboot-elasticsearch-samples/springboot-elasticsearch-basic-samples/src/main/java/cn/chenzw/springboot/elasticsearch/basic/service/SysUserService.java
@@ -1,63 +1,400 @@
 package cn.chenzw.springboot.elasticsearch.basic.service;
 
 import cn.chenzw.springboot.elasticsearch.basic.domain.entity.SysUser;
-import cn.chenzw.springboot.elasticsearch.basic.repository.es.SysUserRepository;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
-import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.admin.indices.alias.Alias;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.IndicesOptions;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.SearchQuery;
+
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SysUserService {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private static final String indexName = "user_index2";
+
     @Autowired
-    SysUserRepository sysUserRepository;
+    private RestHighLevelClient restHighLevelClient;
 
-    public Long save(SysUser sysUser) {
-        SysUser user = sysUserRepository.save(sysUser);
-        return user.getId();
+    /* public Long save(SysUser sysUser) {
+     *//*SysUser user = sysUserRepository.save(sysUser);
+        return user.getId();*//*
+    }*/
+
+
+    /**
+     * 新建索引
+     *
+     * @throws IOException
+     */
+    public void createIndex() throws IOException {
+        CreateIndexRequest request = new CreateIndexRequest(indexName);
+
+        // 配置settings
+        request.settings(
+                Settings.builder()
+                        .put("index.number_of_shards", 3)
+                        .put("index.number_of_replicas", 2)
+        );
+
+        // 创建文档类型映射
+        request.mapping("{\"properties\":{\"userName\":{\"type\":\"text\"},\"age\":{\"type\":\"long\"},\"id\":{\"type\":\"long\"},\"orgId\":{\"type\":\"long\"}}}"
+                , XContentType.JSON);
+
+        // 设置别名
+        request.alias(new Alias("user_index_alias"));
+
+        // 同步执行
+        CreateIndexResponse createIndexResponse = restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
+
+        if (createIndexResponse.isAcknowledged()) {
+            logger.info("所有节点已确认请求!");
+        }
+
+        if (createIndexResponse.isShardsAcknowledged()) {
+            logger.info("在超时之前为索引中的每个分片启动了必需的分片副本数!");
+        }
+
+        // 异步执行
+        /*restHighLevelClient.indices().createAsync(request, RequestOptions.DEFAULT, new ActionListener<CreateIndexResponse>() {
+
+            @Override
+            public void onResponse(CreateIndexResponse createIndexResponse) {
+                // 执行成功回调
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                // 执行失败回调
+            }
+        });*/
+
     }
 
-    public List<SysUser> search(Integer offset,
-                                Integer pageSize,
-                                String searchContent) {
 
-        // 分页
-        Pageable pageable = PageRequest.of(offset, pageSize);
+    /**
+     * 删除索引
+     */
+    public void deleteIndex() throws IOException {
+        DeleteIndexRequest request = new DeleteIndexRequest(indexName);
 
+        //设置IndicesOptions控制如何解决不可用的索引以及如何扩展通配符表达式
+        request.indicesOptions(IndicesOptions.lenientExpandOpen());
 
-        FunctionScoreQueryBuilder functionScoreQueryBuilder = QueryBuilders.functionScoreQuery(QueryBuilders.boolQuery().should(QueryBuilders.matchQuery("username", searchContent)),
-                ScoreFunctionBuilders.weightFactorFunction(1000));
+        // 同步执行
+        AcknowledgedResponse response = restHighLevelClient.indices().delete(request, RequestOptions.DEFAULT);
+        if (response.isAcknowledged()) {
+            logger.info("所有节点都已确认请求!");
+        }
 
-        SearchQuery searchQuery = new NativeSearchQueryBuilder().withPageable(pageable).withQuery(functionScoreQueryBuilder).build();
+        /*restHighLevelClient.indices().deleteAsync(request, RequestOptions.DEFAULT, new ActionListener<AcknowledgedResponse>() {
+            @Override
+            public void onResponse(AcknowledgedResponse acknowledgedResponse) {
+                // 删除成功回调
+            }
 
-        Page<SysUser> searchPageResults = sysUserRepository.search(searchQuery);
-        return searchPageResults.getContent();
-    }
+            @Override
+            public void onFailure(Exception e) {
+                // 删除失败回调
+            }
+        });*/
 
-    List<SysUser> findByUsernameAndState(String username, Byte state) {
-        return sysUserRepository.findByUsernameAndState(username, state);
-    }
-
-    List<SysUser> findByUsernameOrName(String username, String name) {
-        return sysUserRepository.findByUsernameOrName(username, name);
-    }
-
-    Page<SysUser> findByUsername(String username, Pageable pageable) {
-        return sysUserRepository.findByUsername(username, pageable);
-    }
-
-    Page<SysUser> findByUsernameLike(String username, Pageable pageable) {
-        return sysUserRepository.findByUsernameLike(username, pageable);
     }
 
 
+    /**
+     * 字符串形式插入
+     *
+     * @throws IOException
+     */
+    public void insertByString() throws IOException {
+        IndexRequest indexRequest = new IndexRequest(
+                indexName,   // 索引名称
+                "_doc",   // 类型名称
+                "1"   // 文档ID
+        ).source("{\"userName\":\"张三\",\"age\":12,\"orgId\":1,\"id\":1}", XContentType.JSON);
+
+        IndexResponse indexResponse = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+
+        logger.info("Result: {}", indexResponse.getResult());
+        // DocWriteResponse.Result.CREATED => 首次创建文档
+        // DocWriteResponse.Result.UPDATED => 文档被重写
+        logger.info("index: {}， type: {}, id: {}, version: {}",
+                indexResponse.getIndex(),
+                indexResponse.getType(),
+                indexResponse.getId(),
+                indexResponse.getVersion());
+    }
+
+
+    /**
+     * Map形式插入
+     *
+     * @throws IOException
+     */
+    public void insertMap() throws IOException {
+        Map<String, Object> data = new HashMap<>();
+        data.put("userName", "王五");
+        data.put("age", 20);
+        data.put("orgId", 1);
+        data.put("id", 2);
+
+        IndexRequest indexRequest = new IndexRequest(
+                indexName,   // 索引名称
+                "_doc",   // 类型名称
+                "2"   // 文档ID
+        ).source(data);
+        IndexResponse indexResponse = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+
+        logger.info("Result: {}", indexResponse.getResult());
+        // DocWriteResponse.Result.CREATED => 首次创建文档
+        // DocWriteResponse.Result.UPDATED => 文档被重写
+        logger.info("index: {}， type: {}, id: {}, version: {}",
+                indexResponse.getIndex(),
+                indexResponse.getType(),
+                indexResponse.getId(),
+                indexResponse.getVersion());
+    }
+
+    /**
+     * XContent形式插入
+     */
+    public void insertXContent() throws IOException {
+        XContentBuilder builder = XContentFactory.jsonBuilder();
+        builder.startObject();
+
+        builder.field("userName", "赵六");
+        builder.field("age", 12);
+        builder.field("orgId", 34);
+        builder.field("id", 5);
+
+        builder.endObject();
+
+        IndexRequest indexRequest = new IndexRequest(
+                indexName,   // 索引名称
+                "_doc",   // 类型名称
+                "3"   // 文档ID
+        ).source(builder);
+
+        IndexResponse indexResponse = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+
+        logger.info("Result: {}", indexResponse.getResult());
+        // DocWriteResponse.Result.CREATED => 首次创建文档
+        // DocWriteResponse.Result.UPDATED => 文档被重写
+        logger.info("index: {}， type: {}, id: {}, version: {}",
+                indexResponse.getIndex(),
+                indexResponse.getType(),
+                indexResponse.getId(),
+                indexResponse.getVersion());
+    }
+
+    /**
+     * Object形式插入
+     *
+     * @throws IOException
+     */
+    public void insertByObject() throws IOException {
+        IndexRequest indexRequest = new IndexRequest(
+                indexName,   // 索引名称
+                "_doc",   // 类型名称
+                "5"   // 文档ID
+        ).source("userName", "林七", "age", 1, "orgId", 9);
+
+        IndexResponse indexResponse = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+
+        logger.info("Result: {}", indexResponse.getResult());
+        // DocWriteResponse.Result.CREATED => 首次创建文档
+        // DocWriteResponse.Result.UPDATED => 文档被重写
+        logger.info("index: {}， type: {}, id: {}, version: {}",
+                indexResponse.getIndex(),
+                indexResponse.getType(),
+                indexResponse.getId(),
+                indexResponse.getVersion());
+    }
+
+    private void search(QueryBuilder queryBuilder) throws IOException {
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(queryBuilder);
+
+        SearchRequest searchRequest = new SearchRequest(indexName);
+        searchRequest.source(sourceBuilder);
+
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits hits = searchResponse.getHits();
+        long totalHits = hits.getTotalHits();
+        logger.info("总条数: {}", totalHits);
+
+        for (SearchHit hit : hits) {
+            Map source = hit.getSourceAsMap();
+            logger.info(source.toString());
+        }
+    }
+
+
+    /**
+     * 查询所有
+     *
+     * @throws IOException
+     */
+    public void matchAllQuery() throws IOException {
+        // 全量查询
+        QueryBuilder queryBuilder = QueryBuilders.matchAllQuery();
+
+        this.search(queryBuilder);
+    }
+
+    /**
+     * 匹配查询
+     */
+    public void matchQuery() throws IOException {
+        // 模糊查询
+        QueryBuilder queryBuilder = QueryBuilders.matchQuery("userName", "张*");
+
+        this.search(queryBuilder);
+    }
+
+    /**
+     * 精准匹配？
+     */
+    public void commonTermsQuery() throws IOException {
+        QueryBuilder queryBuilder = QueryBuilders.commonTermsQuery("userName", "张");
+        this.search(queryBuilder);
+    }
+
+    /**
+     * 多条件匹配查询
+     */
+    public void multiMatchQuery() throws IOException {
+        // orgId 或 id 字段等于1
+        QueryBuilder queryBuilder = QueryBuilders.multiMatchQuery("1", "orgId", "id");
+        this.search(queryBuilder);
+    }
+
+    /**
+     * 多条件查询union，score沿用子查询score的最大值
+     *
+     * @throws IOException
+     */
+    public void disMaxQuery() throws IOException {
+        DisMaxQueryBuilder queryBuilder = QueryBuilders.disMaxQuery();
+        queryBuilder
+                .add(QueryBuilders.matchQuery("userName", "张"))
+                .add(QueryBuilders.matchQuery("userName", "王"));
+        this.search(queryBuilder);
+    }
+
+    /**
+     * ID查询
+     *
+     * @throws IOException
+     */
+    public void idQuery() throws IOException {
+        IdsQueryBuilder queryBuilder = QueryBuilders.idsQuery();
+        queryBuilder.addIds("1", "2", "5");
+
+        this.search(queryBuilder);
+    }
+
+
+    /**
+     * 分词精确查询
+     */
+    public void termQuery() throws IOException {
+        TermQueryBuilder queryBuilder = QueryBuilders.termQuery("userName", "张三");
+
+        this.search(queryBuilder);
+    }
+
+    /**
+     * 模糊查询
+     */
+    public void fuzzyQuery() throws IOException {
+        FuzzyQueryBuilder queryBuilder = QueryBuilders.fuzzyQuery("userName", "五");
+
+        this.search(queryBuilder);
+    }
+
+    /**
+     * 前缀查询
+     *
+     * @throws IOException
+     */
+    public void prefixQuery() throws IOException {
+        PrefixQueryBuilder queryBuilder = QueryBuilders.prefixQuery("userName", "六");
+
+        this.search(queryBuilder);
+    }
+
+    /**
+     * 范围查询
+     *
+     * @throws IOException
+     */
+    public void rangeQuery() throws IOException {
+        RangeQueryBuilder queryBuilder = QueryBuilders.rangeQuery("age").from(1).to(15)
+                .includeLower(true)  // 包含下界
+                .includeUpper(true); // 包含上界
+
+        this.search(queryBuilder);
+    }
+
+    /**
+     * 通配符查询
+     */
+    public void wildcardQuery() throws IOException {
+        WildcardQueryBuilder queryBuilder = QueryBuilders.wildcardQuery("userName", "张*");
+
+        this.search(queryBuilder);
+    }
+
+    /**
+     * 正则表达式查询
+     */
+    public void regexpQuery() throws IOException {
+        RegexpQueryBuilder queryBuilder = QueryBuilders.regexpQuery("userName", "张");
+
+        this.search(queryBuilder);
+    }
+
+
+    /**
+     *
+     * @throws IOException
+     */
+    public void queryStringQuery() throws IOException {
+        QueryStringQueryBuilder queryBuilder = QueryBuilders.queryStringQuery("2");
+
+        this.search(queryBuilder);
+    }
 }
--- springboot-elasticsearch-samples/springboot-elasticsearch-basic-samples/src/test/java/cn/chenzw/springboot/elasticsearch/basic/service/SysUserServiceTests.java
+++ springboot-elasticsearch-samples/springboot-elasticsearch-basic-samples/src/test/java/cn/chenzw/springboot/elasticsearch/basic/service/SysUserServiceTests.java
@@ -1,90 +1,133 @@
 package cn.chenzw.springboot.elasticsearch.basic.service;
 
-import cn.chenzw.springboot.elasticsearch.basic.ElasticsearchBasicSamplesApp;
 import cn.chenzw.springboot.elasticsearch.basic.domain.entity.SysUser;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.util.List;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = ElasticsearchBasicSamplesApp.class)
-@WebAppConfiguration
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SysUserServiceTests {
 
 
     @Autowired
     SysUserService sysUserService;
 
-    @Test
-    public void testSave() {
-        for (int i = 0; i < 10; i++) {
-            SysUser sysUser = new SysUser();
-            //sysUser.setId(Long.parseLong(String.valueOf(i)));
-            sysUser.setName("张三");
-            sysUser.setUsername("zhangsan");
-            sysUser.setState((byte) 1);
-            sysUser.setPassword("123456");
-            sysUser.setSalt("abc");
 
-            sysUserService.save(sysUser);
-        }
+    /**
+     * 创建索引
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testCreateIndex() throws IOException {
+        sysUserService.createIndex();
     }
 
     /**
-     * 自定义查询字段
+     * 删除索引
+     *
+     * @throws IOException
      */
     @Test
-    public void testSearch() {
-        List<SysUser> sysUsers = sysUserService.search(0, 10, "zhangsan");
-        Assert.assertTrue(sysUsers.size() > 0);
+    public void testDeleteIndex() throws IOException {
+        sysUserService.deleteIndex();
+    }
 
-        List<SysUser> sysUsers2 = sysUserService.search(0, 10, "zhang");
-        Assert.assertTrue(sysUsers2.isEmpty());
+
+    @Test
+    public void testInsertByString() throws IOException {
+        sysUserService.insertByString();
     }
 
     @Test
-    public void testFindByUsernameAndAndState() {
-        List<SysUser> sysUsers = sysUserService.findByUsernameAndState("zhangsan", (byte) 1);
-        Assert.assertTrue(sysUsers.size() > 0);
-
-        List<SysUser> sysUsers2 = sysUserService.findByUsernameAndState("zhangsan", (byte) 0);
-        Assert.assertTrue(sysUsers2.isEmpty());
+    public void testInsertByMap() throws IOException {
+        sysUserService.insertMap();
     }
 
     @Test
-    public void testFindByUsernameOrName() {
-        List<SysUser> sysUsers = sysUserService.findByUsernameOrName("zhangsan", null);
-        Assert.assertTrue(sysUsers.size() > 0);
-
-        List<SysUser> sysUsers2 = sysUserService.findByUsernameOrName(null, "张三");
-        Assert.assertTrue(sysUsers2.size() > 0);
-
+    public void testInsertByXContent() throws IOException {
+        sysUserService.insertXContent();
     }
 
     @Test
-    public void testFindByUsername() {
-        Pageable pageable = PageRequest.of(0, 10);
-        Page<SysUser> sysUserPage = sysUserService.findByUsername("zhangsan", pageable);
-        Assert.assertTrue(sysUserPage.getContent().size() > 0);
-        Assert.assertTrue(sysUserPage.getTotalElements() > 0);
+    public void testInsertXContent() throws IOException {
+        sysUserService.insertXContent();
     }
 
     @Test
-    public void testFindByUsernameLike() {
-        Pageable pageable = PageRequest.of(0, 10);
-        Page<SysUser> sysUserPage = sysUserService.findByUsernameLike("zhang", pageable);
-
-        Assert.assertTrue(sysUserPage.getContent().size() > 0);
-        Assert.assertTrue(sysUserPage.getTotalElements() > 0);
+    public void testInsertByObject() throws IOException {
+        sysUserService.insertByObject();
     }
 
+    @Test
+    public void testMatchAllQuery() throws IOException {
+        sysUserService.matchAllQuery();
+    }
+
+    @Test
+    public void testMatchQuery() throws IOException {
+        sysUserService.matchQuery();
+    }
+
+    @Test
+    public void testCommonTermsQuery() throws IOException {
+        sysUserService.commonTermsQuery();
+    }
+
+    @Test
+    public void testMultiMatchQuery() throws IOException {
+        sysUserService.multiMatchQuery();
+    }
+
+    @Test
+    public void testDisMaxQuery() throws IOException {
+        sysUserService.disMaxQuery();
+    }
+
+    @Test
+    public void testIdQuery() throws IOException {
+        sysUserService.idQuery();
+    }
+
+    @Test
+    public void testTermQuery() throws IOException {
+        sysUserService.termQuery();
+    }
+
+    @Test
+    public void testFuzzyQuery() throws IOException {
+        sysUserService.fuzzyQuery();
+    }
+
+    @Test
+    public void testPrefixQuery() throws IOException {
+        sysUserService.prefixQuery();
+    }
+
+    @Test
+    public void testRangeQuery() throws IOException {
+        sysUserService.rangeQuery();
+    }
+
+    @Test
+    public void testWildcardQuery() throws IOException {
+        sysUserService.wildcardQuery();
+    }
+
+    @Test
+    public void testRegexpQuery() throws IOException {
+        sysUserService.regexpQuery();
+    }
+
+    @Test
+    public void testQueryStringQuery() throws IOException {
+        sysUserService.queryStringQuery();
+    }
 }
